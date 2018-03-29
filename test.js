4886530dc3cb8308f76606a22bf6b1a86a557bed
Adonais0 <chuyao@umich.edu>
2018-03-29 19:14:20 -0400

    add div

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ function gridLayout(points, pointRadius, gridWidth) {
 	return points
 }
 
-function draw(){
+function draw(canvas){
 	//set up canvas
 	const ctx = canvas.node().getContext('2d')
 	ctx.save()
@@ -22,34 +22,48 @@ function draw(){
 		const point = points[i]
 		ctx.fillStyle = 'red'
 		ctx.beginPath();
-      	ctx.arc(point.x, point.y, pointRadius, 0, 2 * Math.PI, false);
+      	ctx.arc(point.x+5, point.y+5, pointRadius, 0, 2 * Math.PI, false);
       	ctx.fill()
 	}
 	ctx.restore()
 }
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width = 960;
+const height = 700;
 const pointRadius = 4;
+
 // create set of points
 const numPoints = 2500
-const points = d3.range(numPoints).map(index =>({
-	id: index,
-	color: 'red'
-}))
+const points = d3.range(numPoints).map(index =>({id: index,color: 'red'}))
 const pointMargin = 10
+
 //create canvas
 const screenScale = window.devicePixelRatio || 1;
-const canvas = d3.select('body').append('canvas')
+const canvas = d3.select('#first').append('canvas')
   .attr('width', width * screenScale)
+  .attr('height', height* screenScale)
+  .style('width', `${width}px`)
+  .style('height', `${height}px`)
+  .attr('margin',20)
+
+canvas.node().getContext('2d').scale(screenScale, screenScale);
+gridLayout(points, pointRadius+pointMargin,width)
+draw(canvas)
+
+//second illustration
+const canvas2 = d3.select('#second').append('canvas')
+  .attr('width', width * screenScale )
   .attr('height', height * screenScale)
   .style('width', `${width}px`)
   .style('height', `${height}px`)
-  .on('click', function () {
-    d3.select('.play-control').style('display', '');
-    timer.stop();
-  });
-canvas.node().getContext('2d').scale(screenScale, screenScale);
+  .attr('margin',20)
 
-
+canvas2.node().getContext('2d').scale(screenScale, screenScale);
 gridLayout(points, pointRadius+pointMargin,width)
-draw()
+draw(canvas2)
+  // .on('click', function () {
+  //   // start the animation
+  //   animate(layouts[currLayout]);
+
+  //   // remove the play control
+  //   d3.select(this).style('display', 'none');
+  // });

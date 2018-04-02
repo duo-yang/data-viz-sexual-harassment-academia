f6db98c160c5ff7e46c89e0f1f72b690902c3f93
duo-yang <yangduots@163.com>
2018-04-02 15:02:14 -0400

    Fix syntax error

--- test.js
+++ test.js
@@ -1,76 +1,76 @@
 function gridLayout(points, pointRadius, gridWidth) {
-	const pointsPerRow = Math.floor(gridWidth / pointRadius)
-	const numRows = points.length / pointsPerRow
+    const pointsPerRow = Math.floor(gridWidth / pointRadius);
+    const numRows = points.length / pointsPerRow;
 
-	points.forEach((point, i) => {
-		point.x = pointRadius * (i % pointsPerRow)
-		point.y = pointRadius* Math.floor(i / pointsPerRow)
-	})
-	return points
+    points.forEach((point, i) => {
+        point.x = pointRadius * (i % pointsPerRow);
+        point.y = pointRadius* Math.floor(i / pointsPerRow)
+    });
+    return points
 }
 
-function draw(canvas,num = 0,color = 'red'){
-	//set up canvas
-	const ctx = canvas.node().getContext('2d')
-	ctx.save()
+function draw(canvas, num=0, color='red'){
+    //set up canvas
+    const ctx = canvas.node().getContext('2d');
+    ctx.save();
 
-	//erase what is on the canvas currently
-	ctx.clearRect(0,0,width, height)
+    //erase what is on the canvas currently
+    ctx.clearRect(0,0,width, height);
 
-	//draw each point
-	for(let i = 0; i<points.length; i++){
-		const point = points[i]
-		if (i<num) 
-			{ctx.fillStyle = 'grey'}
-		else 
-			{ctx.fillStyle = 'red'}
-		ctx.beginPath();
-      	ctx.arc(point.x+5, point.y+5, pointRadius, 0, 2 * Math.PI, false);
-      	ctx.fill()
-	}
-	ctx.restore()
+    //draw each point
+    for(let i = 0; i<points.length; i++){
+        const point = points[i];
+        if (i<num)
+            {ctx.fillStyle = 'grey'}
+        else
+            {ctx.fillStyle = 'red'}
+        ctx.beginPath();
+          ctx.arc(point.x+5, point.y+5, pointRadius, 0, 2 * Math.PI, false);
+          ctx.fill()
+    }
+    ctx.restore()
 }
 const width = 960;
 const height = 700;
 const pointRadius = 4;
 
 // create set of points
-const numPoints = 2500
-const points = d3.range(numPoints).map(index =>({id: index,color: 'red'}))
-const pointMargin = 10
+const numPoints = 2500;
+const points = d3.range(numPoints).map(index =>({id: index,color: 'red'}));
+const pointMargin = 10;
 
 //create canvas
 const screenScale = window.devicePixelRatio || 1;
 const canvas = d3.select('#first').append('canvas')
-  .attr('width', width * screenScale)
-  .attr('height', height* screenScale)
-  .attr('class','vis')
-  .style('width', `${width}px`)
-  .style('height', `${height}px`)
-  .attr('margin',20)
+    .attr('width', width * screenScale)
+    .attr('height', height* screenScale)
+    .attr('class','vis')
+    .style('width', `${width}px`)
+    .style('height', `${height}px`)
+    .attr('margin',20);
 
 canvas.node().getContext('2d').scale(screenScale, screenScale);
-gridLayout(points, pointRadius+pointMargin,width)
-draw(canvas)
+gridLayout(points, pointRadius+pointMargin, width);
+draw(canvas);
 
 //second illustration
 const canvas2 = d3.select('#second').append('canvas')
-  .attr('width', width * screenScale )
-  .attr('height', height * screenScale)
-  .attr('class','vis')
-  .style('width', `${width}px`)
-  .style('height', `${height}px`)
-  .attr('margin',20)
+    .attr('width', width * screenScale )
+    .attr('height', height * screenScale)
+    .attr('class','vis')
+    .style('width', `${width}px`)
+    .style('height', `${height}px`)
+    .attr('margin',20);
 
 canvas2.node().getContext('2d').scale(screenScale, screenScale);
-gridLayout(points, pointRadius+pointMargin,width)
-draw(canvas2, num = 2000)
+gridLayout(points, pointRadius+pointMargin,width);
+draw(canvas2, num = 2000);
 
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-		slidesNavigation: true,
-	});
-	
+    $('#fullpage').fullpage({
+        slidesNavigation: true,
+    });
+
 });
 
   // .on('click', function () {

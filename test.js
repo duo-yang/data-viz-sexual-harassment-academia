61ae8cd69dd4a0b73974485075c6e0b45409fcdf
duo-yang <yangduots@163.com>
2018-04-03 22:19:35 -0400

    Fix syntax issue

--- test.js
+++ test.js
@@ -1,43 +1,43 @@
 function gridLayout(points, pointRadius, gridWidth) {
-	const pointsPerRow = Math.floor(gridWidth / pointRadius);
-	const numRows = points.length / pointsPerRow;
+    const pointsPerRow = Math.floor(gridWidth / pointRadius);
+    const numRows = points.length / pointsPerRow;
 
-	points.forEach(function(point, i) {
-		point.x = pointRadius * (i % pointsPerRow);
-		point.y = pointRadius * Math.floor(i / pointsPerRow);
-	});
-	return points;
+    points.forEach(function(point, i) {
+        point.x = pointRadius * (i % pointsPerRow);
+        point.y = pointRadius * Math.floor(i / pointsPerRow);
+    });
+    return points;
 }
 
 function draw(canvas, num = 0, color = 'red') {
-	// set up canvas
-	const ctx = canvas.node().getContext('2d');
-	ctx.save();
+    // set up canvas
+    const ctx = canvas.node().getContext('2d');
+    ctx.save();
 
-	//erase what is on the canvas currently
-	ctx.clearRect(0,0,width, height);
+    // erase what is on the canvas currently
+    ctx.clearRect(0,0,width, height);
 
-	// draw each point
-	for(let i = 0; i<points.length; i++){
-		const point = points[i];
-		if (i<num) 
-			{ctx.fillStyle = 'grey'}
-		else 
-			{ctx.fillStyle = 'red'}
-		ctx.beginPath();
-      	ctx.arc(point.x+5, point.y+5, pointRadius, 0, 2 * Math.PI, false);
-      	ctx.fill()
-	}
-	ctx.restore()
+    // draw each point
+    for(let i = 0; i<points.length; i++){
+        const point = points[i];
+        if (i<num)
+            {ctx.fillStyle = 'grey'}
+        else
+            {ctx.fillStyle = 'red'}
+        ctx.beginPath();
+          ctx.arc(point.x+5, point.y+5, pointRadius, 0, 2 * Math.PI, false);
+          ctx.fill()
+    }
+    ctx.restore()
 }
 const width = 960;
 const height = 700;
 const pointRadius = 4;
 
 // create set of points
-const numPoints = 2500
-const points = d3.range(numPoints).map(index =>({id: index,color: 'red'}))
-const pointMargin = 10
+const numPoints = 2500;
+const points = d3.range(numPoints).map(index =>({id: index,color: 'red'}));
+const pointMargin = 10;
 
 // create canvas
 const screenScale = window.devicePixelRatio || 1;
@@ -47,7 +47,7 @@ const canvas = d3.select('#first').append('canvas')
   .attr('class','vis')
   .style('width', `${width}px`)
   .style('height', `${height}px`)
-  .attr('margin',20)
+  .attr('margin',20);
 
 canvas.node().getContext('2d').scale(screenScale, screenScale);
 gridLayout(points, pointRadius+pointMargin,width);
@@ -60,40 +60,37 @@ draw(canvas);
 
 
 
-//second illustration
+// second illustration
 const canvas2 = d3.select('#second').append('canvas')
   .attr('width', width * screenScale )
   .attr('height', height * screenScale)
   .attr('class','vis')
   .style('width', `${width}px`)
   .style('height', `${height}px`)
-  .attr('margin',20)
+  .attr('margin',20);
 
 canvas2.node().getContext('2d').scale(screenScale, screenScale);
-gridLayout(points, pointRadius+pointMargin,width)
-draw(canvas2, num = 2000)
+gridLayout(points, pointRadius+pointMargin,width);
+draw(canvas2, num = 2000);
 
-//change color
+// change color
 
 $(document).ready(function() {
 
-	// $('#fullpage').fullpage({
-	// 	slidesNavigation: true,
-	// });
+    // $('#fullpage').fullpage({
+    //     slidesNavigation: true,
+    // });
 
-	// var points = $(".lrg-logo"); 
-	// $(window).scroll(function() {
-	// 	var scroll = $(window).scrollTop();
-
- //    if (scroll >= 500) {
-      
- //    } else {
-      
- //    	}
-	// });
-	$('#fullpage').fullpage({
-		slidesNavigation: true,
-	});
+    // var points = $(".lrg-logo");
+    // $(window).scroll(function() {
+    //     var scroll = $(window).scrollTop();
+    //     if (scroll >= 500) {
+    //    } else {
+    //        }
+    // });
+    $('#fullpage').fullpage({
+        slidesNavigation: true,
+    });
 });
 
   // .on('click', function () {
